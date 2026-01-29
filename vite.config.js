9f10087fb060fb1e0a4bfbd98822568ad30b49ef
hrmnns <hermanns@gmx.net>
2026-01-30 00:12:06 +0100

    1028

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' // Füge das hinzu!
 
 export default defineConfig({
+  base: '/llm-simulator/',
   plugins: [
     react(),
     tailwindcss(), // Und das hier!
