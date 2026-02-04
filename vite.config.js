9256e3c1ecb065ecfce7df0c5eee5c5148b0bea4
hrmnns <hermanns@gmx.net>
2026-02-04 20:31:37 +0100

    1029 renaming

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' // Füge das hinzu!
 
 export default defineConfig({
-  base: '/llm-simulator/',
+  base: '/llm-explorer/',
   plugins: [
     react(),
     tailwindcss(), // Und das hier!
